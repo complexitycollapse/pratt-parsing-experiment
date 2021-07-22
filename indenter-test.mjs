e73e0035b60d52b6ac749b7edd984cae50a77ca5
complexitycollapse <complexitycollapse@protonmail.com>
2021-07-22 17:47:36 +0100

    Indenting

---
+++ indenter-test.mjs
@@ -0,0 +1,39 @@
+import { lexer } from "./lexer.mjs";
+import { indenter, lineFormer } from "./indenter.mjs"
+
+var symbols = {};
+
+function writeTokens(lexer) {
+    var result = document.getElementById("result");
+    result.value = "";
+    try {
+        for(let token = lexer(); !token.eof; token = lexer()) {
+            if (token.sof) continue;
+            result.value += stringifyLine(token);
+            result.value += " ";
+        }
+    }
+    catch (e) {
+        result.value = e;
+    }
+}
+
+function stringifyLine(line) {
+    if (Array.isArray(line)) return JSON.stringify(line) + "\n";
+    else return stringifyToken(line);
+}
+
+function stringifyToken(token) {
+    if (token.symbol) {
+        return "{sym: " + token.text + "}";
+    }
+    else if (token === "\n") {
+        return "{newline}";
+    }
+    else if (token.whitespace) {
+        return "{space: " + token.length + "}";
+    }
+    return token;
+}
+
+document.getElementById("parse").addEventListener("click", () => writeTokens(indenter(lineFormer(lexer()))));
---
+++ indenter.mjs
@@ -0,0 +1,80 @@
+export function lineFormer(lexer) {
+    let eof = undefined;
+    return function lf() {
+        if (eof) return eof;
+        
+        let token = lexer();
+        if (token.sof) return token;
+        
+        let line = [];
+        for(; token != "\n"; token = lexer()) {
+            if (token.eof) {
+                eof = token;
+                break;
+            }
+            line.push(token);
+        }
+        
+        if (line.length === 0 || (line.length === 1 && line[0].whitespace)) {
+            // drop blank lines
+            return lf();
+        }
+
+        line.push("\n");
+        return line;
+    };
+}
+
+export function indenter(lineFormer) {
+    let indentStack = [];
+    let lineNumber = 0;
+
+    function topIndent() { return indentStack[indentStack.length - 1]; };
+
+    return function indent() {
+        let  line = lineFormer();
+        ++lineNumber;
+
+        if (line.sof || line.eof) {
+            return line;
+        }
+
+        let indent = line[0].whitespace ? line[0].length : 0;
+        let prevIndent = topIndent();
+
+        if (prevIndent === undefined) {
+            indentStack.push(indent);
+        }
+
+        if (prevIndent === undefined || indent === prevIndent) {
+            return line[0].whitespace ? line.slice(1) : line;
+        }
+        else if (indent > prevIndent) {
+            line[0] = {indent: true, depth: indentStack.length};
+            indentStack.push(indent);
+            return line;
+        }
+        else {
+            while (indent < topIndent()) {
+                indentStack.pop();
+                if (indentStack.length == 0) {
+                    throw "Pop stack out of bounds " + lineNumber;
+                }
+            }
+
+            if (topIndent() != indent) {
+                throw "Bad indent at line " + lineNumber;
+            }
+            
+            let dedent = {dedent: true, depth: indentStack.length - 1};
+            if (line[0].whitespace) {
+                line[0] = dedent;
+                return line;
+            }
+            else {
+                line.unshift(dedent);
+                return line;
+            }
+        }
+    };
+}
